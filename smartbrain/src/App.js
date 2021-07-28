24e696671c21d5a9041a79262240cd46078febbb
Shar McLeod <shr.mcl@gmail.com>
2021-07-28 13:15:45 -0500

    reverted App to class component, added signup page

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -7,116 +7,6 @@ import Particles from 'react-particles-js';
 import './App.css';
 
 
-// {
-//   "particles": {
-//     "number": {
-//       "value": 104,
-//       "density": {
-//         "enable": true,
-//         "value_area": 1736.124811591
-//       }
-//     },
-//     "color": {
-//       "value": "#ffffff"
-//     },
-//     "shape": {
-//       "type": "circle",
-//       "stroke": {
-//         "width": 0,
-//         "color": "#000000"
-//       },
-//       "polygon": {
-//         "nb_sides": 5
-//       },
-//       "image": {
-//         "src": "img/github.svg",
-//         "width": 100,
-//         "height": 100
-//       }
-//     },
-//     "opacity": {
-//       "value": 0.5,
-//       "random": false,
-//       "anim": {
-//         "enable": false,
-//         "speed": 1,
-//         "opacity_min": 0.1,
-//         "sync": false
-//       }
-//     },
-//     "size": {
-//       "value": 3,
-//       "random": true,
-//       "anim": {
-//         "enable": false,
-//         "speed": 12.181158184520175,
-//         "size_min": 0.1,
-//         "sync": false
-//       }
-//     },
-//     "line_linked": {
-//       "enable": true,
-//       "distance": 150,
-//       "color": "#ffffff",
-//       "opacity": 0.4,
-//       "width": 1
-//     },
-//     "move": {
-//       "enable": true,
-//       "speed": 3.2,
-//       "direction": "none",
-//       "random": false,
-//       "straight": false,
-//       "out_mode": "out",
-//       "bounce": false,
-//       "attract": {
-//         "enable": false,
-//         "rotateX": 600,
-//         "rotateY": 1200
-//       }
-//     }
-//   },
-//   "interactivity": {
-//     "detect_on": "canvas",
-//     "events": {
-//       "onhover": {
-//         "enable": true,
-//         "mode": "repulse"
-//       },
-//       "onclick": {
-//         "enable": true,
-//         "mode": "push"
-//       },
-//       "resize": true
-//     },
-//     "modes": {
-//       "grab": {
-//         "distance": 400,
-//         "line_linked": {
-//           "opacity": 1
-//         }
-//       },
-//       "bubble": {
-//         "distance": 400,
-//         "size": 40,
-//         "duration": 2,
-//         "opacity": 8,
-//         "speed": 3
-//       },
-//       "repulse": {
-//         "distance": 200,
-//         "duration": 0.4
-//       },
-//       "push": {
-//         "particles_nb": 4
-//       },
-//       "remove": {
-//         "particles_nb": 2
-//       }
-//     }
-//   },
-//   "retina_detect": true
-// }
 
 const particlesOptions = {
   "particles": {
@@ -215,18 +105,49 @@ const particlesOptions = {
         },
 }
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition/>*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('submit')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" parms={particlesOptions}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onSubmit={this.onButtonSubmit}/>
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Particles className='particles' params={particlesOptions} />
+//       <Navigation />
+//       <Logo />
+//       <Rank />
+//       <ImageLinkForm />
+//       {/*
+//       <FaceRecognition/>*/}
+//     </div>
+//   );
+// }
 
 export default App;

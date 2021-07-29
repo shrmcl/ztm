f1acd5bd154cc519bb08b77d14a64a7212413170
Shar McLeod <shr.mcl@gmail.com>
2021-07-28 22:50:58 -0500

    replacing project folder and content

---
+++ smartbrain/src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import './App.css';
+
+
+// test ssh
+const particlesOptions = {
+  "particles": {
+    "number": {
+            "value": 204,
+            "density": {
+              "enable": true,
+              "value_area": 1736.124811591
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 3,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 0.5,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "repulse": {
+              "distance": 200,
+              "duration": 3
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('submit')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" parms={particlesOptions}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onSubmit={this.onButtonSubmit}/>
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Particles className='particles' params={particlesOptions} />
+//       <Navigation />
+//       <Logo />
+//       <Rank />
+//       <ImageLinkForm />
+//       {/*
+//       <FaceRecognition/>*/}
+//     </div>
+//   );
+// }
+
+export default App;

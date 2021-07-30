0f5a2fca98884a6f44e3f2f713bfa27a723c7e00
Shar McLeod <shr.mcl@gmail.com>
2021-07-30 16:50:56 -0500

    handled routes for sign in sign out and register

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,6 +4,7 @@ import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -17,13 +18,69 @@ const app = new Clarifai.App({
 const particlesOptions = {
   "particles": {
     "number": {
-            "value": 30,
-            "density": {
-              "enable": true,
-              "value_area": 800
-            }
-          }
-        },
+      "value": 40,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "star",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 6,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "size_min": 4,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": false,
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": true
 }
 
 class App extends Component {
@@ -32,7 +89,9 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -52,10 +111,10 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    // console.log('state box ', this.state.box)
-    // console.log('box ', box)
+    console.log('state box ', this.state.box)
+    console.log('box ', box)
     this.setState({box: box})
-    // console.log('state box 2 ', this.state.box)
+    console.log('state box 2 ', this.state.box)
   }
 
   onInputChange = (e) => {
@@ -66,8 +125,16 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     // this seems to only work with provided sample key: a403429f2ddf4b49b307e318f00e528b
     app.models.predict(
-        Clarifai.FACE_DETECT_MODEL, // which clarifai model we use
-        this.state.input) // url of image being passed to api
+      // Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+      // Check Face Detect Mode: https://www.clarifai.com/models/face-detection
+      // If that isn't working, then that means you will have to wait until their servers are back up. OR
+      // use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+      // change from:
+      // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      // to:
+      // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+      Clarifai.FACE_DETECT_MODEL, // which clarifai model we use
+      this.state.input) // url of image being passed to api
     .then(response => {
       // console.log('response on submit: ', response)
       this.displayFaceBox(this.calculateFaceLocaction(response))
@@ -75,36 +142,40 @@ class App extends Component {
     .catch(err => console.log(err))
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
   render() {
-    console.log('state of the box obj: ', this.state.box)
+    const { isSignedIn, imageUrl, route, box} = this.state;
+    // console.log('state of the box obj: ', this.state.box)
     return (
       <div className="App">
-        <Particles className="particles" parms={particlesOptions}/>
-        <Navigation />
-        <Signin />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onSubmit={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Particles className="particles" params={particlesOptions}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+            route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} /> 
+            : <Register onRouteChange={this.onRouteChange} /> 
+            )
+        }
       </div>
     )
   }
 }
-
-// function App() {
-//   return (
-//     <div className='App'>
-//       <Particles className='particles' params={particlesOptions} />
-//       <Navigation />
-//       <Logo />
-//       <Rank />
-//       <ImageLinkForm />
-//       {/*
-//       <FaceRecognition/>*/}
-//     </div>
-//   );
-// }
 
 export default App;
